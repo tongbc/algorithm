370e39c7effe3273ca35cc8b67c5f208504b0f1d
“tongbc <624360737@qq.com>
2018-07-27 17:23:48 +0800

    second4

---
+++ src/justForReal/LongestSubstring.java
@@ -0,0 +1,23 @@
+package justForReal;
+
+import java.util.HashMap;
+
+public class LongestSubstring {
+	public static int lengthOfLongestSubstring(String s) {
+		if (s.length() == 0)
+			return 0;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		int max = 0;
+		for (int i = 0, j = 0; i < s.length(); ++i) {
+			if (map.containsKey(s.charAt(i))) {
+				j = Math.max(j, map.get(s.charAt(i)) + 1);
+			}
+			map.put(s.charAt(i), i);
+			max = Math.max(max, i - j + 1);
+		}
+		return max;
+	}
+	public static void main(String[] args) {
+		System.out.println(LongestSubstring.lengthOfLongestSubstring("abcabcbb"));
+	}
+}
