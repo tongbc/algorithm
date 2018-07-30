2d02fab6ecf791f974debb093f57d68a4c16dcb4
“tongbc <624360737@qq.com>
2018-07-30 15:49:09 +0800

    7/30

---
+++ src/justForReal/RelativeRanks.java
@@ -0,0 +1,39 @@
+package justForReal;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RelativeRanks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {1,2,3};
+		System.out.println(RelativeRanks.findRelativeRanks(nums));
+	}
+	
+	public static String[] findRelativeRanks(int[] nums) {
+		String[] result = new String[nums.length];
+        int[] tmp = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(tmp);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = tmp.length - 1; i >= 0; i--) {
+            // score -> ranking
+            map.put(tmp[i], tmp.length - i);
+        }
+
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) == 1) {
+                result[i] = "Gold Medal";
+            } else if (map.get(nums[i]) == 2) {
+                result[i] = "Silver Medal";
+            } else if (map.get(nums[i]) == 3) {
+                result[i] = "Bronze Medal";
+            } else {
+                result[i] = String.format("%s", map.get(nums[i]));
+            }
+        }
+        return result;
+	}
+}
