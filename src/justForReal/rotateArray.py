4e3c8b55603d7b39224d467b5e86e336b11a11e0
“tongbc <624360737@qq.com>
2019-05-14 14:58:14 +0800

    today

---
+++ src/justForReal/rotateArray.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if k == 0:
+            return
+        k = k%len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+
+    def reverse(self, nums, start, end):
+        while (start < end):
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1
