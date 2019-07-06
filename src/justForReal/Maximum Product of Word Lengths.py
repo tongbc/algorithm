263ec50536c543e3fa230ebe94066c0b91cd2bc5
“tongbc <624360737@qq.com>
2019-07-06 22:05:15 +0800

    二进制

---
+++ src/justForReal/Maximum Product of Word Lengths.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProduct(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        d = {}
+        for w in words:
+            mask = 0
+            for c in set(w):
+                mask |= 1<<(ord(c)-ord("a"))
+            d[mask] = max(d.get(mask,0),len(w))
+        return max([d[x]*d[y] for x in d for y in d if not x&y] or [0])
