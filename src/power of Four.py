79c69e75d1c6a934ffdc1558a24546079c2665d2
tongbc <624360737@qq.com>
2022-03-04 10:20:41 +0800

    test

--- src/power of Four.py
+++ src/power of Four.py
@@ -2,6 +2,6 @@ class Solution(object):
     def isPowerOfFour(self, num):
         """
         :type num: int
-        :rtype: bool111
+        :rtype: bool1112222
         """
         return num != 0 and num &(num-1) == 0 and num & 1431655765== num
